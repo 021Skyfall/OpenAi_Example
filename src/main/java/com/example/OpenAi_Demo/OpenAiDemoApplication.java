df1072db04993949bcd70e53f36574468556fb71
jerry <021skyfall@gmail.com>
2024-09-26 13:10:43 +0900

    project base

---
+++ src/main/java/com/example/OpenAi_Demo/OpenAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenAi_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OpenAi_Demo/OpenAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenAi_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
